35d5e83bed0e20158615b1c46c70d05684e5d1bc
Timo Koriseva <timo.koriseva@futuriot.com>
2018-11-23 16:05:14 +0200

    Modify

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -16864,7 +16864,7 @@ global.$ = require("jquery");
 require('bootstrap');
 require('popper.js');
 
-var uri = "https://prod-19.northeurope.logic.azure.com/workflows/6890be1a9c384658b836fefd7f38311d/triggers/manual/paths/invoke/desucon?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=9KNnfgr6FhBy4ujw2mhSw0hx8h_KdTTpLD1M_xPwza0";
+var getTweetsUri = "https://prod-19.northeurope.logic.azure.com/workflows/6890be1a9c384658b836fefd7f38311d/triggers/manual/paths/invoke/desucon?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=9KNnfgr6FhBy4ujw2mhSw0hx8h_KdTTpLD1M_xPwza0";
 
 
 $( document ).ready(function() {
@@ -16872,7 +16872,7 @@ $( document ).ready(function() {
     var content = "";
 
     $.ajax({
-        url: uri,
+        url: getTweetsUri,
         type: "GET",
         contentType:"application/json; charset=utf-8",
         dataType: "json",
@@ -16902,7 +16902,7 @@ function createCard(text, user, time, image)
             '<p class="card-text">' + text + '</p>' +
         '</div>' +
         '<div class="card-footer">' +
-            '<small class="text-muted">' + user + ' - ' + (new Date(time)).toLocaleString() + '</small>' +
+            '<small class="text-muted"><a href="https://twitter.com/' + user + '">' + user + '</a> - ' + (new Date(time)).toLocaleString() + '</small>' +
         '</div>' +
     '</div>';
 }

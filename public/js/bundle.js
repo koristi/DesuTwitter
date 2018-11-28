be15b6c98317b133dbb815f0566493428ba0d2e3
Timo Koriseva <timo.koriseva@futuriot.com>
2018-11-28 10:49:55 +0200

    Modify

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -16864,14 +16864,18 @@ global.$ = require("jquery");
 require('bootstrap');
 require('popper.js');
 
-var getTweetsUri = "https://prod-19.northeurope.logic.azure.com/workflows/6890be1a9c384658b836fefd7f38311d/triggers/manual/paths/invoke/desucon?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=9KNnfgr6FhBy4ujw2mhSw0hx8h_KdTTpLD1M_xPwza0";
-
+var getTweetsUri = "https://prod-19.northeurope.logic.azure.com/workflows/" + 
+    "6890be1a9c384658b836fefd7f38311d/triggers/manual/paths/invoke/desucon" + 
+    "?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=9KNnfgr6FhBy4ujw2mhSw0hx8h_KdTTpLD1M_xPwza0";
+var getTweetDataUri = "https://prod-28.northeurope.logic.azure.com/workflows/" + 
+    "fa9c0a3b26304b6c8ce04a06de25cbde/triggers/manual/paths/invoke/tweets" + 
+    "?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=KiSu4ob7IqBHqjboZqfOJ5b78mwzKXBrmqv3FOosucM";
 
 $( document ).ready(function() {
 
     var content = "";
 
-    $.ajax({
+/*     $.ajax({
         url: getTweetsUri,
         type: "GET",
         contentType:"application/json; charset=utf-8",
@@ -16883,14 +16887,26 @@ $( document ).ready(function() {
             });
 
             $(".card-columns").html(content);
+        }
+    }); */
 
+    $.ajax({
+        url: getTweetDataUri,
+        type: "GET",
+        contentType:"application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+            drawCountPerPerson(response);
+            drawCountPerDate(response);
+            drawCountPerHour(response);
+            getAllWords(response);
         }
     });
 });
 
-function createCard(text, user, time, image)
-{
-    var cardElementInit = "<div class='card tweet'>"; 
+function createCard(text, user, time, image) {
+    var cardElementInit = "<div class='card'>"; 
 
     if (image.length > 0)
     {
@@ -16902,9 +16918,185 @@ function createCard(text, user, time, image)
             '<p class="card-text">' + text + '</p>' +
         '</div>' +
         '<div class="card-footer">' +
-            '<small class="text-muted"><a href="https://twitter.com/' + user + '">' + user + '</a> - ' + (new Date(time)).toLocaleString() + '</small>' +
+            '<small class="text-muted"><a href="https://twitter.com/' + user + '">' + 
+            user + '</a> - ' + (new Date(time)).toLocaleString() + '</small>' +
         '</div>' +
     '</div>';
+}
+
+function drawCountPerPerson(response) {
+    var chart = echarts.init(document.getElementById('countChart'));
+
+    var perUserArray = [];
+    var tweetCounts = [];
+    var users = [];
+    
+    response.forEach(tweet => {
+
+        if (tweetCounts[tweet.User] != null)
+        {
+            tweetCounts[tweet.User] += 1;
+        }
+        else
+        {
+            tweetCounts[tweet.User] = 1;
+        }
+
+        if (!users.includes(tweet.User))
+        {
+            users.push(tweet.User);
+        }
+    });
+
+    users.forEach(username => {
+        perUserArray.push({value: [username, tweetCounts[username]]});
+    });
+
+    console.log(perUserArray);
+
+    var option = {
+        title: {
+            text: 'Tweets Per User'
+        },
+        color: 'green',
+        xAxis: {
+            type: 'category'
+        },
+        yAxis: {
+            type: 'value',
+            minInterval: 1
+        },
+        series: {
+            type: 'bar',
+            data: perUserArray
+        }
+    };
+
+    chart.setOption(option);
+}
+
+function drawCountPerDate(response) {
+    var chart = echarts.init(document.getElementById('dateChart'));
+
+    var perDateArray = [];
+    var tweetCounts = [];
+    var dates = [];
+    
+    response.forEach(tweet => {
+
+        var y = new Date(tweet.Created).getFullYear();
+        var m = new Date(tweet.Created).getMonth();
+        var d = new Date(tweet.Created).getDate();
+        var valueAsDate = new Date(y,m,d);
+
+        if (tweetCounts[valueAsDate.toLocaleDateString()] != null)
+        {
+            tweetCounts[valueAsDate.toLocaleDateString()] += 1;
+        }
+        else
+        {
+            tweetCounts[valueAsDate.toLocaleDateString()] = 1;
+        }
+
+        if (!dates.includes(valueAsDate.toLocaleDateString()))
+        {
+            dates.push(valueAsDate.toLocaleDateString());
+        }
+    });
+
+    dates.forEach(date => {
+        perDateArray.push({value: [date, tweetCounts[date]]});
+    });
+
+    console.log(perDateArray);
+
+    var option = {
+        title: {
+            text: 'Tweets Per Date'
+        },
+        color: 'green',
+        xAxis: {
+            type: 'category'
+        },
+        yAxis: {
+            type: 'value',
+            minInterval: 1
+        },
+        series: {
+            type: 'bar',
+            data: perDateArray
+        }
+    };
+
+    chart.setOption(option);
+}
+
+function drawCountPerHour(response) {
+    var chart = echarts.init(document.getElementById('hourChart'));
+
+    var perHourArray = [];
+    var tweetCounts = [];
+    var hours = [];
+    
+    response.forEach(tweet => {
+
+        var y = new Date(tweet.Created).getFullYear();
+        var m = new Date(tweet.Created).getMonth();
+        var d = new Date(tweet.Created).getDate();
+        var h = new Date(tweet.Created).getHours();
+        var valueAsDate = new Date(y,m,d,h);
+
+        if (tweetCounts[valueAsDate.toLocaleString()] != null)
+        {
+            tweetCounts[valueAsDate.toLocaleString()] += 1;
+        }
+        else
+        {
+            tweetCounts[valueAsDate.toLocaleString()] = 1;
+        }
+
+        if (!hours.includes(valueAsDate.toLocaleString()))
+        {
+            hours.push(valueAsDate.toLocaleString());
+        }
+    });
+
+    hours.forEach(date => {
+        perHourArray.push({value: [date, tweetCounts[date]]});
+    });
+
+    console.log(perHourArray);
+
+    var option = {
+        title: {
+            text: 'Tweets Per Hour'
+        },
+        color: 'green',
+        xAxis: {
+            type: 'category'
+        },
+        yAxis: {
+            type: 'value',
+            minInterval: 1
+        },
+        series: {
+            type: 'bar',
+            data: perHourArray
+        }
+    };
+
+    chart.setOption(option);
+}
+
+function getAllWords(response)
+{
+    var arrayOfWords = [];
+    response.forEach(tweet => {
+        arrayOfWords = arrayOfWords.concat(tweet.Text.split(' '));
+    });
+
+    drawMeACloud(arrayOfWords);
+    placeWords();
 }
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"bootstrap":1,"jquery":2,"popper.js":3}]},{},[4]);
---
+++ public/js/word-cloud.js
@@ -0,0 +1,160 @@
+/*jshint esversion: 6 */
+
+/*  ======================= SETUP ======================= */
+var config = {
+    trace: false,
+    spiralResolution: 1, //Lower = better resolution
+    spiralLimit: 360 * 5,
+    lineHeight: 0.8,
+    xWordPadding: 0,
+    yWordPadding: 3,
+    font: "Calibri"
+};
+
+var words;
+var startPoint;
+var cloud;
+var wordsDown;
+var mostFrequent = [];
+
+function getMostFrequent(allWords)
+{
+    const arr1=allWords;
+    let mf = 1;
+    let m = 0;
+    let item;
+    for (let i=0; i<arr1.length; i++)
+    {
+            for (let j=i; j<arr1.length; j++)
+            {
+                    if (arr1[i] == arr1[j])
+                     m++;
+                    if (mf<=m)
+                    {
+                      mf=m; 
+                      item = arr1[i];
+                    }
+            }
+            m=0;
+    }
+    console.log(`${item} ( ${mf} times ) `) ;
+
+    if (allWords.length > 0 && item != undefined && mostFrequent.length < 10)
+    {
+        mostFrequent.push(item);
+        var newWords = allWords.filter(w => w != item);
+        getMostFrequent(newWords);
+    }
+}
+
+function drawMeACloud(allwords)
+{
+    getMostFrequent(allwords);
+    
+    words = mostFrequent.map(function(word) {
+        return {
+            word: word,
+            freq: Math.floor((1-0.1*mostFrequent.indexOf(word)) * 50) + 10
+        };
+    });
+
+    words.sort(function(a, b) {
+        return -1 * (a.freq - b.freq);
+    });
+
+    cloud = document.getElementById('word-cloud');
+    cloud.style.position = "relative";
+    cloud.style.fontFamily = config.font;
+
+    startPoint = {
+        x: cloud.offsetWidth / 2,
+        y: cloud.offsetHeight / 2
+    };
+
+    wordsDown = [];
+    /* ======================= END SETUP ======================= */
+}
+
+/* =======================  PLACEMENT FUNCTIONS =======================  */
+function createWordObject(word, freq) {
+    var wordContainer = document.createElement("div");
+    wordContainer.style.position = "absolute";
+    wordContainer.style.fontSize = freq + "px";
+    wordContainer.style.lineHeight = config.lineHeight;
+/*    wordContainer.style.transform = "translateX(-50%) translateY(-50%)";*/
+    wordContainer.appendChild(document.createTextNode(word));
+
+    return wordContainer;
+}
+
+function placeWord(word, x, y) {
+
+    cloud.appendChild(word);
+    word.style.left = x - word.offsetWidth/2 + "px";
+    word.style.top = y - word.offsetHeight/2 + "px";
+
+    wordsDown.push(word.getBoundingClientRect());
+}
+
+function trace(x, y) {
+//     traceCanvasCtx.lineTo(x, y);
+//     traceCanvasCtx.stroke();
+    //traceCanvasCtx.fillRect(x, y, 1, 1);
+}
+
+function spiral(i, callback) {
+    angle = config.spiralResolution * i;
+    x = (1 + angle) * Math.cos(angle);
+    y = (1 + angle) * Math.sin(angle);
+    return callback ? callback() : null;
+}
+
+function intersect(word, x, y) {
+    cloud.appendChild(word);    
+    
+    word.style.left = x - word.offsetWidth/2 + "px";
+    word.style.top = y - word.offsetHeight/2 + "px";
+    
+    var currentWord = word.getBoundingClientRect();
+    
+    cloud.removeChild(word);
+    
+    for(var i = 0; i < wordsDown.length; i+=1){
+        var comparisonWord = wordsDown[i];
+        
+        if(!(currentWord.right + config.xWordPadding < comparisonWord.left - config.xWordPadding ||
+             currentWord.left - config.xWordPadding > comparisonWord.right + config.wXordPadding ||
+             currentWord.bottom + config.yWordPadding < comparisonWord.top - config.yWordPadding ||
+             currentWord.top - config.yWordPadding > comparisonWord.bottom + config.yWordPadding)){
+            
+            return true;
+        }
+    }
+    
+    return false;
+}
+/* =======================  END PLACEMENT FUNCTIONS =======================  */
+
+
+
+
+
+/* =======================  LETS GO! =======================  */
+function placeWords() {
+    for (var i = 0; i < words.length; i += 1) {
+
+        var word = createWordObject(words[i].word, words[i].freq);
+
+        for (var j = 0; j < config.spiralLimit; j++) {
+            //If the spiral function returns true, we've placed the word down and can break from the j loop
+            if (spiral(j, function() {
+                    if (!intersect(word, startPoint.x + x, startPoint.y + y)) {
+                        placeWord(word, startPoint.x + x, startPoint.y + y);
+                        return true;
+                    }
+                })) {
+                break;
+            }
+        }
+    }
+}
